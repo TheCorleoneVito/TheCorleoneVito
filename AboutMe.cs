63cb811b6a9a6ac594c9d7627aaae5c27908b55e
TheCorleoneVito <105847720+TheCorleoneVito@users.noreply.github.com>
2022-05-19 14:31:35 +0430

    Create AboutMe.cs

---
+++ AboutMe.cs
@@ -0,0 +1,19 @@
+using Global;
+
+namespace AboutMe
+{
+public class AboutForm1 : Form
+ {
+public AboutForm1()
+        {
+            InitializeComponent();
+        }
+        
+  public void ButtonAbout_Click(object sender , EventArgs vito)
+  {
+    Label1.text = "❓ Hi My Name Is Mohammad And My Nickname Is VitoCorleone Because I Very Love The GodFather(Seris 1)";
+    Label2.text = "👨‍💻 My Favorite Programming Language : C# For Ever";
+    label3.text = "⚡ I Love Games & Codes";
+  }
+
+ }
